45b7681c61d9c62c483fd35b2a15376d701b1351
Bobby Finley <rmfinley@yahoo.com>
2025-02-25 16:47:16 -0500

    Refactor GetAuthorDetailsAsync for better organization
    Refactored GetAuthorDetailsAsync in BookSearchService.cs to improve code organization and readability. Extracted HTTP client configuration and author details fetching logic into new private methods: ConfigureHttpClient and FetchAuthorDetailsAsync. This separation of concerns enhances modularity, maintainability, and consistency.

--- SOCFrontEnd/SOCFrontEnd.Server/Services/BookSearchService.cs
+++ SOCFrontEnd/SOCFrontEnd.Server/Services/BookSearchService.cs
@@ -19,17 +19,7 @@ namespace SOCDataManager.Services
         {
             try
             {
-                using var client = _httpClientFactory.CreateClient();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Add("User-Agent", "SOCFrontEnd");
-                client.BaseAddress = new Uri("https://openlibrary.org/");
-
-                var response = await client.GetAsync($"search.json?q={authorName}");
-                response.EnsureSuccessStatusCode();
-
-                var jsonResponse = await response.Content.ReadAsStringAsync();
-                var author = JsonConvert.DeserializeObject<Author>(jsonResponse);
-
+                var author = await FetchAuthorDetailsAsync(authorName);
                 return author;
             }
             catch (Exception ex)
@@ -37,6 +27,27 @@ namespace SOCDataManager.Services
                 _logger.LogError(ex, "An error occurred while querying the OpenLibraryAPI.");
                 return null;
             }
+        }
+
+        private async Task<Author?> FetchAuthorDetailsAsync(string authorName)
+        {
+            using var client = _httpClientFactory.CreateClient();
+            ConfigureHttpClient(client);
+             
+            var response = await client.GetAsync($"search.json?q={authorName}");
+            response.EnsureSuccessStatusCode();
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var author = JsonConvert.DeserializeObject<Author>(jsonResponse);
+
+            return author;
+        }
+
+        private void ConfigureHttpClient(HttpClient client)
+        {
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "SOCFrontEnd");
+            client.BaseAddress = new Uri("https://openlibrary.org/");
         }
     }
 }
